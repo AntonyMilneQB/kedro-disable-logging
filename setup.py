3ed9c1003bdb9d4bbfe757a5551536bc75dab1dd
Antony Milne <antony.milne@quantumblack.com>
2021-12-02 18:13:59 +0000

    Simplify

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="kedro-disable-logging",
     version="0.1",
+    py_modules=["plugin"],
     entry_points={
         "kedro.init": ["disable_logging = plugin:disable_logging"],
     },
